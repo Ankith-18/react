5d876efc8a09b22b3fab36a4098cbbbf10759333
Ankit Kumar <ankitkr3012005@gmail.com>
2026-05-27 15:55:28 +0530

    Fix formatting in AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react"
 const AuthContext = createContext()
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null) // null = not logged in
+  const [user, setUser] = useState(null) 
 
   function login(dummyUser) {
     setUser(dummyUser)
